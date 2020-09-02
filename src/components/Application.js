e89e9dc90a2b23aa2e1c4a874345029dc229b32a
NB-famous <ncbadoles@gmail.com>
2020-09-02 18:00:40 -0400

    added useEffect to Application to be able to fetch data from api for days content using axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 
@@ -61,28 +62,27 @@ const appointments = [
 
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+
+  useEffect(() => {
+
+    console.log("Fetching Data.....")
+
+    axios({
+      url:`/api/days`,
+      method:'GET'
+    })
+    .then(result => {
+      setDays(result.data)
+    })
+    .catch(err => console.log(err));
+
+  }, [])
 
   return (
     <main className="layout">
