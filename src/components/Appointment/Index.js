c83fb85fb119994240c103d68318f7f9244e97ac
NB-famous <ncbadoles@gmail.com>
2020-09-02 08:51:13 -0400

    Refactored Appointment/Index.js && added story to test this functionality

--- src/components/Appointment/Index.js
+++ src/components/Appointment/Index.js
@@ -10,7 +10,10 @@ const Appointment = (props) =>{
 
     return (
 
-        <article className="appointment"></article>
+        <article className="appointment">
+            <Header time={props.time}/>
+            {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/>}
+        </article>
     )
 }
 
